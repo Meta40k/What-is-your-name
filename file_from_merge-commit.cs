65affcbb893001e6fff8ca5d0aab322008aa5127
LopatkinDA <LopatkinDA@quarta.su>
2023-04-04 17:47:07 +0300

    create file merge-commit

---
+++ file_from_merge-commit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace What_is_your_name
+{
+    internal class file_from_merge_commit
+    {
+    }
+}
