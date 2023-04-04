26012b76d7c3053a89c2631a94af447a54b40022
LopatkinDA <LopatkinDA@quarta.su>
2023-04-04 17:48:34 +0300

    two merge commit

---
+++ two merge commit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace What_is_your_name
+{
+    internal class two_merge_commit
+    {
+    }
+}
